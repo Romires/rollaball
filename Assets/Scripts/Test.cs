dc60c3719026426a26af273b39e42ae199e91294
Romires <qsort@yandex.ru>
2022-04-14 20:03:32 +0300

    lesson1

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Geekbrains
+{
+    public class Test : MonoBehaviour
+    {
+        private void Start()
+        {
+            var objects = new ListInteractableObject();
+
+            for (int i = 0; i < objects.Count; i++)
+            {
+                print($"{objects[i]}");
+            }
+        }
+    }
+}
